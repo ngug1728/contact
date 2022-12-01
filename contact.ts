084bdc739c729859c1eb495eeafe312704b3fa72
ngug1728 <humblesys09@outlook.com>
2022-12-01 15:42:00 +0300

    props

--- contact.ts
+++ contact.ts
@@ -1,4 +1,10 @@
 const root = ReactDOM.createRoot(document.getElementById('root'))
 export function Contact({black,white}){
-
 }
+export default function App(){
+< Contact
+	black=""
+	white=""
+/>
+}
+
